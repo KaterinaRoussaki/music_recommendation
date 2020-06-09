5c2fd8f871e4d8c6284599bc1c26c805eb8cb5c2
katerina <katerinarouss@gmail.com>
2020-06-10 01:58:09 +0300

    import the ds class

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
+from DemsterShafer import DempsterShafer as Ds
 from collect_data_functions import return_all_item_set_bpa_dicts
-from Demster_Shafer_functions import (
-    ds_mc_combination_rule,
-    mass_functions_from_dictionaries,
-)
 
 
 def main():
     dictionaries = return_all_item_set_bpa_dicts()
 
-    mass_functions = mass_functions_from_dictionaries(dictionaries)
-    final_mass_function = ds_mc_combination_rule(mass_functions)
+    mass_functions = Ds.mass_functions_from_dictionaries(dictionaries)
+    final_mass_function = Ds.ds_mc_combination_rule(mass_functions)
 
     print(final_mass_function)
 
