11ab724f80655fe68c30b14007fcbec87e76367a
katerina roussaki <katerinarouss@gmail.com>
2020-10-17 16:29:20 +0300

    changed the coments

--- DemsterShafer.py
+++ DemsterShafer.py
@@ -2,17 +2,14 @@ from pyds import MassFunction
 
 
 class DempsterShafer:
-    """
-
-    Returns an array with mass_functions of a array of dictionaries
-
-    @:param array of dictionaries
-    @:returns array of mass functions
-
-    """
-
     @staticmethod
     def mass_functions_from_dictionaries(dictionary_array):
+        """
+        Returns an array with mass_functions of a array of dictionaries
+
+        :param dictionary_array:
+        :returns array of mass functions
+        """
         mass_function_array = []
 
         for dictionary in dictionary_array:
@@ -20,28 +17,20 @@ class DempsterShafer:
 
         return mass_function_array
 
-    """
-        Returns the mass function after using the Dempster Shafer combination rule
-        
-        @:param array of mass functions
-        @:returns dictionary of mass functions, Dempster Shafer
-    """
-
     @staticmethod
     def ds_combination_rule(mass_function_array):
+        """
+        Returns the mass function after using the Dempster Shafer combination rule
+
+        :param mass_function_array:
+        :returns dictionary of mass functions, Dempster Shafer
+        """
         dempster_shafer = mass_function_array[0] & mass_function_array[1]
 
         for mass_function in mass_function_array[2:]:
             dempster_shafer = dempster_shafer & mass_function
 
         return dempster_shafer
-
-    """
-        Returns the mass function after using the Dempster Shafer combination rule via using monte carlo 
-        
-        @:param array of mass functions
-        @:returns dictionary of mass functions, monte carlo 
-    """
 
     @staticmethod
     def ds_mc_combination_rule(
@@ -50,6 +39,15 @@ class DempsterShafer:
         importance_sampling=True,
         normalization=True,
     ):
+        """
+        Returns the mass function after using the Dempster Shafer combination rule via using monte carlo
+
+        :param normalization:
+        :param importance_sampling:
+        :param sample_count:
+        :param mass_function_array:
+        :returns dictionary of mass functions, monte carlo
+        """
         monte_carlo = mass_function_array[0] & mass_function_array[1]
 
         for mass_function in mass_function_array[2:]:
@@ -62,58 +60,44 @@ class DempsterShafer:
 
         return monte_carlo
 
-    """
-        Make a query by using an item dictionary and find it's belief
-        
-        @:param dictionary with items
-        @:param mass function
-        @:returns float belief 
-    """
-
     @staticmethod
     def return_belief(item_dict, mass_function):
-        return mass_function.bel(item_dict)
+        """
+        Make a query by using an item dictionary and find it's belief
 
-    """
-        Return the entire belief function
-        
-        @:param dictionary with items
-        @:returns dictionary 
-    """
+        :param mass_function:
+        :param item_dict:
+        :returns float belief
+        """
+        return mass_function.bel(item_dict)
 
     @staticmethod
     def return_entire_belief(mass_function):
-        return mass_function.bel()
+        """
+        Return the entire belief function
 
-    """
-        Make a query by using an item dictionary and find it's plausibility
-        
-        @:param string dictionary
-        @:param mass function
-        @:returns float plausibility 
-    """
+        :param mass_function:
+        :returns dictionary
+        """
+        return mass_function.bel()
 
     @staticmethod
     def return_plausibility(item_dict, mass_function):
-        return mass_function.pl(item_dict)
+        """
+        Make a query by using an item dictionary and find it's plausibility
 
-    """
-        Return the entire belief function
-        
-        @:param dictionary with items
-        @:returns dictionary 
-    """
+        :param mass_function:
+        :param item_dict:
+        :returns float plausibility
+        """
+        return mass_function.pl(item_dict)
 
     @staticmethod
     def return_entire_plausibility(mass_function):
-        return mass_function.pl()
+        """
+        Return the entire belief function
 
-    """
-        Returns true if the desirability of an item set is greater than the threshold
-    
-        @:param mass function dictionary
-        @:param threshold for the desirability
-        @:param conservation_degree
-        @:param item_set
-        @:returns boolean 
-    """
+        :param mass_function:
+        :returns dictionary
+        """
+        return mass_function.pl()
