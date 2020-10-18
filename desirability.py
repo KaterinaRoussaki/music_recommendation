a38f4cee49516b3b67541cb3ce48d3f040591b87
katerina roussaki <katerinarouss@gmail.com>
2020-10-18 09:18:52 +0300

    added an average score and the selection to return the results for both methods

--- desirability.py
+++ desirability.py
@@ -1,6 +1,8 @@
 import numpy as np
+import time
 
 from DemsterShafer import DempsterShafer as Ds
+from general_functions import print_song_sets, print_to_file
 
 
 def desirability(conservation_degree, belief, plausibility):
@@ -33,7 +35,7 @@ def find_desirable_sets(mass_functions, threshold, conservation_degree=0):
             item_set = item_set.union(key)
             score_array.append(score)
 
-    return item_set
+    return item_set, score_array
 
 
 def find_set_desirability(mass_function, item_set, conservation_degree, threshold=0.5):
@@ -54,3 +56,65 @@ def find_set_desirability(mass_function, item_set, conservation_degree, threshol
         return True, des
     else:
         return False, None
+
+
+def average_score(score_array):
+    """
+    Returns the average scores of all songs
+
+    :param score_array:
+    :return:
+    """
+    song_num = len(score_array)
+
+    scores = 0
+    for score in score_array:
+        scores += score
+
+    return scores / float(song_num)
+
+
+def return_results(
+    file_name,
+    mass_functions,
+    conservation_degree,
+    threshold,
+    song_data,
+    is_monte_carlo=True,
+):
+    """
+    Returns the results in a .txt file
+
+    :param file_name:
+    :param mass_functions:
+    :param conservation_degree:
+    :param threshold:
+    :param song_data:
+    :param is_monte_carlo:
+    :return:
+    """
+    start = time.perf_counter()
+    if is_monte_carlo:
+        final_mass_function = Ds.ds_mc_combination_rule(mass_functions)
+    else:
+        final_mass_function = Ds.ds_combination_rule(mass_functions)
+
+    final_item_sets, score_array = find_desirable_sets(
+        final_mass_function, threshold, conservation_degree
+    )
+    end = time.perf_counter()
+
+    result = ""
+    result += "The conservation degree: "
+    result += str(conservation_degree)
+    result += "\nThe songs that we suggest to the users are the following: \n"
+    result += print_song_sets(song_data, final_item_sets)
+    result += "\nThe scores of the songs: \n"
+    result += str(score_array)
+    result += "\nAverage song score: "
+    result += str(average_score(score_array))
+    result += "\nTime: "
+    result += str(end - start)
+    result += "\n"
+
+    print_to_file(file_name, result)
